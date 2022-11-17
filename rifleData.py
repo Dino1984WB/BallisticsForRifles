57fd1cb05fdeb3b12abf10b74cd1f225795a2364
Williamkbukowski <>
2022-11-17 04:19:34 -0500

    init commit of ballistics cleaning and tranforming python program

---
+++ rifleData.py
@@ -0,0 +1,33 @@
+import csv
+import pandas as pd
+
+def main():
+    
+    #begin pandas stuff
+    #grab file, produce a dataframe of certain columns only
+    input_file = "C:\\DataAnalytics\\Ballistics.csv"
+    dataset = pd.read_csv(input_file)
+    df = pd.DataFrame(dataset)
+
+    cols = [2,3,4,5,6]
+    dfc = df[df.columns[cols]]
+
+    dfc.astype(float) #this works!!! woohoo!!
+
+    print(dfc.to_string())
+    print("----------------------------------------\n")
+    print("Above is unsorted list, below sorted by bullet weight")
+    print("-----------------------------------------\n")
+
+
+    #dfc is a dataframe that can contain mass in grains of projectiles
+    #1000 > n > 0, but range is practically speaking smaller
+    #cut off at/including row 75
+    dfg = dfc.sort_values("Weight")
+    print(dfg.to_string())
+
+    dfg.to_csv('CleanedBallisticsData.csv')
+
+
+
+main()
