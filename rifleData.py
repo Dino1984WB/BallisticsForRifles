a713770e2d2b9d8a52fb05dd23d8975597e5209e
Williamkbukowski <>
2022-11-17 08:49:51 -0500

    finished the getData function, now produces cleaned and ordered data as is required for the graphing to begin. very happy with results

--- rifleData.py
+++ rifleData.py
@@ -6,14 +6,7 @@ import pandas as pd
 def getData():
 
     #begin pandas stuff
-    #grab files, produce a dataframe rom the csv files
-
-    #imports all, only rifles, and then only hangun's ballistics
-    #import Ballistics.csv
-    
-    #input_file_ballistics = "C:\\DataAnalytics\\Ballistics.csv"
-    #datasetAll = pd.read_csv(input_file_ballistics)
-    #dfA = pd.DataFrame(datasetAll)
+    #grab files, produce a dataframe rom the csv files 
     
     #import rifleCarts.csv
     input_file_rifle = "C:\\DataAnalytics\\rifleCartscsv.csv"
@@ -24,13 +17,10 @@ def getData():
     datasetHandgun = pd.read_csv(input_file_handgun)
     dfH = pd.DataFrame(datasetHandgun)
 
-
-    #remove unneeded cloumns from dataframes
-    cols = [0,1,2]
-    #dfcA = dfA[dfA.columns[cols]]
-    dfcR = dfR[dfR.columns[cols]]
-    dfcH = dfH[dfH.columns[cols]]
-
+    #remove unneeded columns from the two dataframes
+    cols = [0,1,2] #selects columns with array counting
+    dfcR = dfR[dfR.columns[cols]] #selects specific columns for Rifle
+    dfcH = dfH[dfH.columns[cols]] #selects specific columns for Handgun
 
     #converts values of dfc dataframe to float (likely from string or int)
     #dfcA.astype(float)
@@ -39,30 +29,31 @@ def getData():
 
     #print the dataframe dfc
     #print(dfcA.to_string())
-    print(dfcR.to_string())
-    print(dfcH.to_string())
+    #print(dfcR.to_string())
+    #print(dfcH.to_string())
 
     #Delimiter thingy that devides the nonsorted and sorted dataframes
-    print("----------------------------------------\n")
-    print("Above is unsorted list, below sorted by bullet weight")
-    print("-----------------------------------------\n")  
-    
+    print("\n\n\n\n")
+    print("Below is the data sorted by bullet weight:")
+    print("\n\n\n\n\n")
 
     #dfc is a dataframe that can contain mass in grains of projectiles
+    printThisR = dfcR.sort_values(dfcR.columns[0]) #sorts the Rifle records numerically
+    printThisH = dfcH.sort_values(dfcH.columns[0]) #sorts the Handgun records numerically
     
-    dfcR.sort_values(dfcR.columns[1])
-    dfcH.sort_values(dfcH.columns[1])
-    
-    print(dfcR.to_string())
-    print(dfcH.to_string())
+    #prints the dataframes to the console
+    print(printThisR.to_string())
+    print("^Rifle data, above | below, Handgun data")
+    print(printThisH.to_string())
 
-    
     #send dataframe into a csv file
-    dfcR.to_csv('CleanedBallisticRifleData.csv')
-    dfcH.to_csv('CleanedBallisticHandgunData.csv')
+    #dfcR.to_csv('CleanedBallisticRifleData.csv')
+    #dfcH.to_csv('CleanedBallisticHandgunData.csv')
 
     #waits for user input to hold open window
     pauseForever = input("Press any key to Continue")
+
+
 
 getData()
 
